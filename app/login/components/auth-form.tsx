cfa44a70d5c7e8249f13ca658fa5084e11ab586b
Ernesto Junior <ernesto.sjunior@hotmail.coms>
2023-12-18 21:55:45 -0300

    wip: create login page layout

---
+++ app/login/components/auth-form.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+interface AuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function AuthForm({ className, ...props }: AuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              E-mail
+            </Label>
+            <Input
+              id="email"
+              placeholder="e-mail"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+            <Label className="sr-only" htmlFor="email">
+              Senha
+            </Label>
+            <Input
+              id="email"
+              placeholder="senha"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>Entrar</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
